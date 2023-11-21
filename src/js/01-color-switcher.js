6de9947e3be986167efc8d13d4734080b9e1ab70
StealthM0de <jedongfallorina@gmail.com>
2023-11-21 18:30:58 +0800

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
 
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function changeBackgroundColor() {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+}
+
+startButton.addEventListener('click', () => {
+  if (intervalId === null) {
+    intervalId = setInterval(changeBackgroundColor, 1000);
+    startButton.disabled = true;
+  }
+});
+
+stopButton.addEventListener('click', () => {
+  if (intervalId !== null) {
+    clearInterval(intervalId);
+    intervalId = null;
+    startButton.disabled = false;
+  }
+});
